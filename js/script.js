2dda4503a5db7e730a8c20ddfb27eae4560156b4
katy01101011 <katy.91560@gmail.com>
2022-04-11 03:31:49 +0200

    non funziona

--- js/script.js
+++ js/script.js
@@ -1,55 +1,79 @@
-// Buttons
-const formGetInfo = document.getElementById('genera');
-console.log(formGetInfo);
-formGetInfo.addEventListener('click',
+// BOTTONE GENERA
+const getInfoBtn = document.getElementById('genera');
+console.log(getInfoBtn);
+getInfoBtn.addEventListener('click',
     function() {
+    // Mostro Biglietto
+        const ticketContainer = document.querySelector(".ticket");
+        ticketContainer.classList.add("d-block");
+
     // Prelievo input utente
-        // const passengerName = document.getElementById('passenger-name').value;
-        // console.log(passengerName);
 
-        // const passengerDistance = document.getElementById('passenger-distance');
-        // console.log(passengerDistance);
+        const passengerName = document.getElementById('passenger-name').value;
+        console.log(passengerName);
 
-        // const ticketType = document.getElementById("passenger-age").value;
-        // console.log(ticketType);
+        const passengerDistance = document.getElementById('passenger-distance').value;
+        console.log(passengerDistance);
 
-// // Visualizzazione in HTML risposte utente
-        // const nameResponse = document.getElementById('passenger-name');
-        // console.log(nameResponse);
-        // nameResponse.innerHTML = `${passengerName}`;
+        const ticketType = document.getElementById("passenger-age").value;
+        console.log(ticketType);
 
-        // const ageResponse = document.getElementById('passenger-age');
-        // console.log(ageResponse);
-        // ageResponse.innerHTML = `${passengerAge}`;
+    // Visualizzazione in HTML risposte utente
+        // NOME PASSEGGERO
+        const nameResponse = document.getElementById('passenger-name');
+        console.log(nameResponse);
+        nameResponse.innerHTML = `${passengerName}`;
 
-    // Qual'è il prezzo del biglietto del treno?
-        // const standardTicket = passengerDistance * 0.21;
-        // console.log(standardTicket, typeof(regularTicket));
+        // OFFERTA
+        const ageResponse = document.getElementById('ticket-type');
+        console.log(ageResponse);
+        ageResponse.innerHTML = `${ticketType}`;
+        
+        // CARROZZA
+            const ticketWagon = Math.floor(Math.random() * (9) + 1)
+            console.log(ticketWagon);
 
-        // if (userAge <= 18) {
-        //     ticketPrice *= 0.8;
-        //     console.log(ticketPrice);
-        // } else if (userAge > 65) {
-        //     ticketPrice *= 0.6;
-        //     console.log(ticketPrice);
-        // }
+            const wagonResponse = document.getElementById('ticket-wagon');
+            console.log(wagonResponse);
+            wagonResponse.innerHTML = `${ticketWagon}`;
 
-    // document.getElementById('ticket-price').innerHTML = ticketPrice.toFixed(2);       
+        // CODICE CP
+            const ticketCpCode = Math.floor(Math.random() * (99999 - 90000 + 1) + 90000)
+            console.log(ticketCpCode);
+
+            const cpCodeResponse = document.getElementById('ticket-cp-code');
+            console.log(cpCodeResponse);
+            cpCodeResponse.innerHTML = `${ticketCpCode}`;
+
+        // Qual'è il prezzo del biglietto del treno?
+            const standardTicket = passengerDistance * 0.21;
+            console.log(standardTicket, typeof(regularTicket));
+
+            if (userAge <= 18) {
+                ticketPrice *= 0.8;
+                console.log(ticketPrice);
+            } else if (userAge > 65) {
+                ticketPrice *= 0.6;
+                console.log(ticketPrice);
+            }
+            document.getElementById('ticket-price').innerHTML = ticketPrice.toFixed(2);
     }
-)
+);
 
-// CARROZZA
-const ticketWagon = Math.floor(Math.random() * (9) + 1)
-console.log(ticketWagon);
 
-const wagonResponse = document.getElementById('ticket-wagon');
-        console.log(wagonResponse);
-        wagonResponse.innerHTML = `${ticketWagon}`;
+// BOTTONE ANNULLA
+        const backBtn = document.getElementById('annulla');
+        console.log(backBtn);
+        backBtn.addEventListener('click',
+            function() {
 
-// CODICE CP
-const ticketCpCode = Math.floor(Math.random() * (99999 - 90000 + 1) + 90000)
-console.log(ticketCpCode);
+                // Nascondo Biglietto
+                const ticketContainer = document.querySelector(".ticket");
+                ticketContainer.classList.add("d-none");
 
-const cpCodeResponse = document.getElementById('ticket-cp-code');
-        console.log(cpCodeResponse);
-        cpCodeResponse.innerHTML = `${ticketCpCode}`;
+                // Nascondo i campi compilati
+                const passengerName = document.getElementById('passenger-name');
+                console.log(passengerName);
+                passengerName.value = '';
+            }
+        )
